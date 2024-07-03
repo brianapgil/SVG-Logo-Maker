019941cffe6af8510d0a028831d1868f52991f1b
Briana Gil <brianapgil@gmail.com>
2024-07-03 13:33:55 -0400

    Updated index.js file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const readline = require('readline-sync');
-const generateSVG = require('./lib/shapes')
+const generateSVG = require('./lib/shapes');
 
 function main() {
-    const shape = readline.question('Enter shape choice (circle, triangle, or square)');
+    const shapes = ['circle', 'triangle', 'square'];
+    const shape = readline.keyInSelect(shapes, 'Select a shape.', {cancel:false});
+    const shapeindex = shapes[shape];
     const text = readline.question('Enter text for the logo (up to 3 letters).');
     const color = readline.question('Enter the color of the logo.');
     const textcolor = readline.question('Enter the color of the text for the logo.');
